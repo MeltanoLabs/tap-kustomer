1886a8fc8ead64bb39571345805167427d136bde
jared-rimmer <jared.rimmer@brooklyndata.co>
2023-04-13 14:02:30 +0000

    Refactor note stream to .json file

--- tap_kustomer/streams/note_streams.py
+++ tap_kustomer/streams/note_streams.py
@@ -23,21 +23,9 @@ class NoteStream(CustomerSearchStream):
     primary_keys = ["id"]
     replication_key = "updated_at"
     records_jsonpath = "$[data][*]"
+    schema_filepath = SCHEMAS_DIR / "note.json"
 
     max_observed_timestamp = None
     max_timestamp = None
     updated_at = "note_updated_at"
     query_context = "note"
-
-    schema = th.PropertiesList(
-        th.Property("type", th.StringType, description=""),
-        th.Property("id", th.StringType, description=""),
-        th.Property("updated_at", th.DateTimeType, description=""),
-        th.Property("attributes", 
-            th.ObjectType(
-                th.Property("body", th.StringType, description=""),
-                th.Property("createdAt", th.StringType, description=""),
-                th.Property("updatedAt", th.StringType, description=""),
-                th.Property("modifiedAt", th.StringType, description=""),
-                th.Property("createdByTeams", th.ArrayType(th.StringType), description=""),
-        ))).to_dict()
